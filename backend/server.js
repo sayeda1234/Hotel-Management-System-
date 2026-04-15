4924ed460ac6ef1eaee2616102c93cf8e783067c
sayeda <you@example.com>
2026-04-15 10:26:31 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,10 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Backend is running ✅");
+});
+
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/admin', adminRoutes);
